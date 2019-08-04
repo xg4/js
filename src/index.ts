2f06dfa850b57b289d9900cb5be6d1e94383a3b0
xg4 <xingor4@gmail.com>
2019-08-04 19:27:48 +0800

    chore: add babel, typescript, jest

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+interface Function {
+  _call(this: Function, thisArg: any, ...argArray: any[]): any;
+
+  _apply(this: Function, thisArg: any, argArray?: any): any;
+
+  _bind(this: Function, thisArg: any, ...argArray: any[]): any;
+}
+
+Function.prototype._call = function(context = window, ...args) {
+  // 生成唯一的 key ，避免其他属性被覆盖
+  const key = Symbol()
+  // 绑定 ctx 的 this
+  context[key] = this
+  // 获得结果
+  const result = context[key](...args)
+  // 删除 ctx 上的函数
+  delete context[key]
+  return result
+}
