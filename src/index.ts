31e3355ae96fba2f544d9977107ad75e6816401e
xg4 <xingor4@gmail.com>
2019-08-05 19:27:48 +0800

    chore: add eslint

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 interface Function {
-  _call(this: Function, thisArg: any, ...argArray: any[]): any;
+  _call(this: Function, thisArg: any, ...argArray: any[]): any
 
-  _apply(this: Function, thisArg: any, argArray?: any): any;
+  _apply(this: Function, thisArg: any, argArray?: any): any
 
-  _bind(this: Function, thisArg: any, ...argArray: any[]): any;
+  _bind(this: Function, thisArg: any, ...argArray: any[]): any
 }
 
+// eslint-disable-next-line no-extend-native
 Function.prototype._call = function(context = window, ...args) {
   // 生成唯一的 key ，避免其他属性被覆盖
-  const key = Symbol()
+  const key = Symbol('fn key')
   // 绑定 ctx 的 this
   context[key] = this
   // 获得结果
